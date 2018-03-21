bba619e36c415829affd601f3b957e4e16336283
SamDev <soumyad@appcino.com>
2018-03-21 14:03:34 +0530

    " ..

--- controllers/ivr.js
+++ controllers/ivr.js
@@ -2,7 +2,7 @@ const Twilio 	= require('twilio')
 
 const taskrouterHelper = require('./helpers/taskrouter-helper.js')
 //const voice_settings = "{voice: 'woman',language: 'no'}";
-var voice_settings = JSON.parse({voice: 'woman',language: 'nb-NO'})
+//var voice_settings = JSON.parse({voice: 'woman',language: 'nb-NO'})
 
 module.exports.welcome = function (req, res) {
 	const twiml =  new Twilio.twiml.VoiceResponse()
@@ -31,7 +31,7 @@ module.exports.welcome = function (req, res) {
 
 	twiml.say({
 	    voice: 'woman',
-	    language: 'no',
+	    language: 'nb-NO',
 	  },'Du sa ikke noe eller angir noen siffer.')
 	twiml.pause({length: 2})
 	twiml.redirect({method: 'GET'}, 'welcome')
@@ -79,7 +79,7 @@ module.exports.selectTeam = function (req, res) {
 	/* the caller pressed a key that does not match any team */
 	if (team === null) {
 		// redirect the call to the previous twiml
-		twiml.say(voice_settings,'Ditt valg var ikke gyldig, prøv igjen.')
+		twiml.say({voice: 'woman',language: 'nb-NO'},'Ditt valg var ikke gyldig, prøv igjen.')
 		twiml.pause({length: 2})
 		twiml.redirect({ method: 'GET' }, 'welcome')
 	} else {
@@ -91,7 +91,7 @@ module.exports.selectTeam = function (req, res) {
 			timeout: 5
 		})
 
-		gather.say(voice_settings,'Trykk på en tast hvis du vil ha tilbakering fra ' + team.friendlyName + ', Eller bli på linjen.')
+		gather.say({voice: 'woman',language: 'nb-NO'},'Trykk på en tast hvis du vil ha tilbakering fra ' + team.friendlyName + ', Eller bli på linjen.')
 
 		/* create task attributes */
 		const attributes = {
@@ -129,10 +129,10 @@ module.exports.createTask = function (req, res) {
 
 	taskrouterHelper.createTask(req.configuration.twilio.workflowSid, attributes)
 		.then(task => {
-			twiml.say(voice_settings,'Takk for tilbakeringingsanmodningen din, En agent vil ringe deg tilbake snart.')
+			twiml.say({voice: 'woman',language: 'nb-NO'},'Takk for tilbakeringingsanmodningen din, En agent vil ringe deg tilbake snart.')
 			twiml.hangup()
 		}).catch(error => {
-			twiml.say(voice_settings,'beklager, farvel!') //sorry bye //'An application error occured, the demo ends now'
+			twiml.say({voice: 'woman',language: 'nb-NO'},'beklager, farvel!') //sorry bye //'An application error occured, the demo ends now'
 		}).then(() => {
 			res.send(twiml.toString())
 		})
